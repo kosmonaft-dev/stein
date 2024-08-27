cb53d5b9e38e033c08604ab8d02fe84bcf8989dd
debuss-a <alexandre@kosmonaft.dev>
2024-08-27 21:22:37 +0200

    upd(docker): removed comments

--- src/Infrastructure/Pokemon/Client/PokemonClient.php
+++ src/Infrastructure/Pokemon/Client/PokemonClient.php
@@ -4,6 +4,7 @@ namespace Stein\Infrastructure\Pokemon\Client;
 
 use GuzzleHttp\Client;
 use JsonMapper;
+use Psr\Http\Message\ResponseInterface;
 use Stein\Framework\Http\Error\ApiCallException;
 use Stein\Infrastructure\Pokemon\Dto\{PokemonDto, PokemonListDto};
 use function json_decode;
@@ -18,15 +19,7 @@ class PokemonClient
 
     public function getPokemonList(int $offset, int $limit): PokemonListDto
     {
-        $response = $this->client->request('GET', '/api/v2/pokemon?'.http_build_query(['offset' => $offset, 'limit' => $limit]));
-        if ($response->getStatusCode() !== 200) {
-            $exception = new ApiCallException('An error occurred.');
-            $exception->status_code = $response->getStatusCode();
-            $exception->reason_phrase = $response->getReasonPhrase();
-
-            throw $exception;
-        }
-
+        $response = $this->callApi('/api/v2/pokemon?'.http_build_query(['offset' => $offset, 'limit' => $limit]));
         $results = json_decode($response->getBody()->getContents());
 
         return $this->mapper->map($results, PokemonListDto::class);
@@ -34,17 +27,23 @@ class PokemonClient
 
     public function getPokemonById(int $id): PokemonDto
     {
-        $response = $this->client->request('GET', '/api/v2/pokemon/'.$id);
+        $response = $this->callApi('/api/v2/pokemon/'.$id);
+        $result = json_decode($response->getBody()->getContents());
+
+        return $this->mapper->map($result, PokemonDto::class);
+    }
+
+    private function callApi(string $uri): ResponseInterface
+    {
+        $response = $this->client->request('GET', $uri);
         if ($response->getStatusCode() !== 200) {
-            $exception = new ApiCallException('An error occurred.');
+            $exception = new ApiCallException('An error occurred with the API call to '.$uri);
             $exception->status_code = $response->getStatusCode();
             $exception->reason_phrase = $response->getReasonPhrase();
 
             throw $exception;
         }
 
-        $result = json_decode($response->getBody()->getContents());
-
-        return $this->mapper->map($result, PokemonDto::class);
+        return $response;
     }
 }
